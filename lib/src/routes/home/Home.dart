cd24dcd563e0c4275907c89f470b696eb6b7b5aa
Ramon Paolo Maran <ramonpaolomaran12@gmail.com>
2020-10-04 15:15:10 -0300

    Att FutureBuilder(not work offline now)

--- lib/src/routes/home/Home.dart
+++ lib/src/routes/home/Home.dart
@@ -34,8 +34,8 @@ class _HomeState extends State<Home> {
 
   Future money() async {
     try {
-      //response = await http.get("https://api.hgbrasil.com/finance");
-      response = await http.get("https://google");
+      response = await http.get("https://api.hgbrasil.com/finance");
+      //response = await http.get("https://google");
       var json = await jsonDecode(response.body);
 
       dolar = await json["results"]["currencies"]["USD"]["buy"];
@@ -50,9 +50,9 @@ class _HomeState extends State<Home> {
 
   Future wheater(woeid) async {
     try {
-      //response = await http
-      //    .get("https://api.hgbrasil.com/weather?woeid=$woeid&key=63c27cec");
-      response = await http.get("https://google");
+      response = await http
+          .get("https://api.hgbrasil.com/weather?woeid=$woeid&key=63c27cec");
+      //response = await http.get("https://google");
       tempo = await jsonDecode(response.body);
       print("API Tempo:");
       print(await tempo);
@@ -64,9 +64,9 @@ class _HomeState extends State<Home> {
 
   Future localizatio() async {
     try {
-      //response = await http.get(
-      //"https://api.hgbrasil.com/geoip?key=63c27cec&address=remote&precision=false");
-      response = await http.get("https://google");
+      response = await http.get(
+          "https://api.hgbrasil.com/geoip?key=63c27cec&address=remote&precision=false");
+      //response = await http.get("https://google");
       localization = await jsonDecode(response.body);
       print("API localização:");
       print(await localization);
@@ -114,7 +114,6 @@ class _HomeState extends State<Home> {
   void initState() {
     // TODO: implement initState
     datas();
-    json();
     super.initState();
   }
 
@@ -124,7 +123,7 @@ class _HomeState extends State<Home> {
     return Container(
         child: FutureBuilder(
       builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.active) {
+        if (snapshot.hasData) {
           return SingleChildScrollView(
             child: Column(
               children: [
@@ -312,183 +311,6 @@ class _HomeState extends State<Home> {
         } else if (snapshot.connectionState == ConnectionState.waiting) {
           return Center(
             child: CircularProgressIndicator(),
-          );
-        } else {
-          return SingleChildScrollView(
-            child: Column(
-              children: [
-                Padding(
-                  padding:
-                      EdgeInsets.only(top: 40, left: 20, right: 20, bottom: 10),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(40),
-                    child: Container(
-                      child: TextField(
-                        controller: _pesquisa,
-                        onChanged: (value) {
-                          setState(() {
-                            animated = false;
-                          });
-                          search(_pesquisa.text);
-                          value = _pesquisa.text;
-                          if (value.isEmpty) {
-                            setState(() {
-                              animated = true;
-                              pesquisa = null;
-                            });
-                          }
-                        },
-                        showCursor: true,
-                        strutStyle: StrutStyle(leading: 0.4),
-                        keyboardType: TextInputType.text,
-                        cursorColor: Colors.green[900],
-                        textCapitalization: TextCapitalization.sentences,
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          prefixIcon: Icon(
-                            Icons.search,
-                            color: Colors.green,
-                          ),
-                          suffixIcon: pesquisa != null
-                              ? IconButton(
-                                  icon: Icon(
-                                    Icons.clear,
-                                    color: Colors.green,
-                                  ),
-                                  onPressed: () {
-                                    setState(() {
-                                      _pesquisa.clear();
-                                      animated = true;
-                                      pesquisa = null;
-                                    });
-                                  })
-                              : null,
-                        ),
-                      ),
-                      decoration: BoxDecoration(color: Colors.white),
-                    ),
-                  ),
-                ),
-                AnimatedCrossFade(
-                    firstChild: Container(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(
-                            Icons.wb_sunny,
-                            color: Colors.yellow,
-                            size: 88.0,
-                          ),
-                          Padding(
-                            child: Column(children: [
-                              Row(
-                                children: [
-                                  Text(
-                                    "Max: 30 ºC",
-                                    style: TextStyle(
-                                        color: Colors.white, fontSize: 18),
-                                  ),
-                                  Text(
-                                    "  Min: 16 ºC",
-                                    style: TextStyle(
-                                        color: Colors.white, fontSize: 18),
-                                  ),
-                                ],
-                              ),
-                              Text(
-                                "Itapeva - SP",
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 18),
-                              ),
-                              Text(
-                                "R\$5.65 um dólar",
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 16),
-                              )
-                            ]),
-                            padding: EdgeInsets.only(left: 24),
-                          )
-                        ],
-                      ),
-                    ),
-                    secondChild: Column(children: [
-                      Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            pesquisa == null
-                                ? Text("Pesquisando...",
-                                    style: TextStyle(
-                                        color: Colors.white, fontSize: 18))
-                                : Text("")
-                          ]),
-                      pesquisa != null
-                          ? Text("Achado!",
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 18))
-                          : Text("")
-                    ]),
-                    crossFadeState: animated
-                        ? CrossFadeState.showFirst
-                        : CrossFadeState.showSecond,
-                    duration: Duration(milliseconds: 700)),
-                Text("Esse está sem conecção"),
-                RefreshIndicator(
-                  child: ClipRRect(
-                      borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(60),
-                          topRight: Radius.circular(60)),
-                      child: Container(
-                          height: size.height <= 700
-                              ? size.height * 0.62 + (animated ? 0 : 51)
-                              : size.height * 0.7 + (animated ? 0 : 70),
-                          color: Colors.white,
-                          padding: EdgeInsets.only(left: 10, right: 10),
-                          child: pesquisa != null
-                              ? Padding(
-                                  padding: EdgeInsets.all(20),
-                                  child: ClipRRect(
-                                      borderRadius: BorderRadius.circular(25),
-                                      child: Card(
-                                        child: Column(
-                                          children: [
-                                            Stack(
-                                              textDirection: TextDirection.rtl,
-                                              children: [
-                                                Image.asset(
-                                                  "${planta["image"]}",
-                                                  filterQuality:
-                                                      FilterQuality.high,
-                                                  fit: BoxFit.fill,
-                                                ),
-                                                IconButton(
-                                                    icon: Icon(
-                                                      Icons.favorite,
-                                                      color: planta["favorite"]
-                                                          ? Colors.green
-                                                          : Colors.white,
-                                                    ),
-                                                    onPressed: () {
-                                                      setState(() {
-                                                        planta["favorite"] =
-                                                            !planta["favorite"];
-                                                      });
-                                                    }),
-                                              ],
-                                            ),
-                                            ListTile(
-                                              title: Text("${planta["title"]}"),
-                                              subtitle:
-                                                  Text("${planta["subtitle"]}"),
-                                            ),
-                                          ],
-                                        ),
-                                      )))
-                              : construtor(size, plantas))),
-                  onRefresh: json,
-                ),
-              ],
-            ),
           );
         }
       },
