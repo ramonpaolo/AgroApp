de8a9b2c95d1c7b4add68b5670cb3ddef5b3bbd4
Ramon Paolo Maran <ramonpaolomaran12@gmail.com>
2020-10-04 09:02:43 -0300

    Add login automatic

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 //---- Packages
 import 'package:flutter/material.dart';
 import 'package:simple_splashscreen/simple_splashscreen.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
 
 //---- Screens
 import 'package:agricultura/src/models/Nav.dart';
@@ -21,12 +25,29 @@ class Index extends StatefulWidget {
 }
 
 class _IndexState extends State<Index> {
+  Future<File> _getData() async {
+    final directory = await getApplicationDocumentsDirectory();
+    final file = File("${directory.path}/data.json");
+    return file;
+  }
+
+  Future _readData() async {
+    try {
+      final file = await _getData();
+      final decode = jsonDecode(file.readAsStringSync());
+      print(decode);
+      return decode;
+    } catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Simple_splashscreen(
         context: context,
         splashscreenWidget: Splash(),
-        gotoWidget: Login(),
+        gotoWidget: _readData() != null ? Nav() : Login(),
         timerInSeconds: 2);
   }
 }
