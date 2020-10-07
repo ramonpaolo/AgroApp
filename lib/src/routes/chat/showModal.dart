d776717c911a1583b2aa9a0a5033d0d671209924
Ramon Paolo Maran <ramonpaolomaran12@gmail.com>
2020-10-06 21:44:39 -0300

    Add a modal of drazer

--- lib/src/routes/chat/showModal.dart
+++ lib/src/routes/chat/showModal.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:agricultura/src/routes/chat/PageHero.dart';
 
 showModal(context, imagem, galery) {
   return showModalBottomSheet(
@@ -43,5 +45,67 @@ showModal(context, imagem, galery) {
                 ],
               ),
             ));
+      });
+}
+
+showModalDrazer(_images, context, widget) {
+  return showModalBottomSheet(
+      context: context,
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(40), topRight: Radius.circular(40))),
+      builder: (c) {
+        return ClipRRect(
+            borderRadius: BorderRadius.circular(40),
+            child: Container(
+                height: 1000,
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Divider(
+                        color: Colors.white,
+                      ),
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(200),
+                        child: Image.asset(
+                          widget.image,
+                          width: 200,
+                        ),
+                      ),
+                      Text(
+                        widget.name,
+                        style: TextStyle(fontSize: 24),
+                      ),
+                      SizedBox(
+                          width: 1000,
+                          height: 130,
+                          child: ListView.builder(
+                            scrollDirection: Axis.horizontal,
+                            itemBuilder: (context, index) {
+                              return Padding(
+                                  padding: EdgeInsets.only(left: 20),
+                                  child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(30),
+                                      child: Container(
+                                          width: 100,
+                                          child: GestureDetector(
+                                            onTap: () => Navigator.push(
+                                                context,
+                                                MaterialPageRoute(
+                                                    builder: (context) =>
+                                                        PageHero(
+                                                            _images[index]))),
+                                            child: Hero(
+                                                tag: index,
+                                                child: Card(
+                                                    child: Image.file(
+                                                        File(_images[index])))),
+                                          ))));
+                            },
+                            itemCount: _images.length,
+                          ))
+                    ],
+                  ),
+                )));
       });
 }
