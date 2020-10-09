a59d8e8b9a3d909dc243124340149bcb91d59035
Ramon Paolo Maran <ramonpaolomaran12@gmail.com>
2020-10-09 14:57:20 -0300

    Add categories

---
+++ lib/src/data/category.dart
@@ -0,0 +1,7 @@
+List category = [
+  {"name": "Cosméticos", "image": "assets/images/cosmeticos.jpg"},
+  {"name": "Adubos", "image": "assets/images/cosmeticos.jpg"},
+  {"name": "Peças", "image": "assets/images/cosmeticos.jpg"},
+  {"name": "Eletrônicos", "image": "assets/images/cosmeticos.jpg"},
+  {"name": "Plantas", "image": "assets/images/cosmeticos.jpg"}
+];
