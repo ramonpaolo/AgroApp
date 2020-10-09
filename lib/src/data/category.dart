46cdfb5ae94a65fd0ded369a30032e99e9ac362e
Ramon Paolo Maran <ramonpaolomaran12@gmail.com>
2020-10-09 18:51:09 -0300

    Att images

--- lib/src/data/category.dart
+++ lib/src/data/category.dart
@@ -1,7 +1,7 @@
 List category = [
   {"name": "Cosméticos", "image": "assets/images/cosmeticos.jpg"},
-  {"name": "Adubos", "image": "assets/images/cosmeticos.jpg"},
-  {"name": "Peças", "image": "assets/images/cosmeticos.jpg"},
-  {"name": "Eletrônicos", "image": "assets/images/cosmeticos.jpg"},
-  {"name": "Plantas", "image": "assets/images/cosmeticos.jpg"}
+  {"name": "Adubos", "image": "assets/images/adubo.jpg"},
+  {"name": "Ferramenta", "image": "assets/images/ferramenta.jpg"},
+  {"name": "Eletrônicos", "image": "assets/images/drone.jpg"},
+  {"name": "Ter na casa", "image": "assets/images/ter_casa.webp"},
 ];
