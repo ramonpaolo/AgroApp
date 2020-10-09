fb1ff9c6545321ce7e65b199a0c40b2ccac3cc77
Ramon Paolo Maran <ramonpaolomaran12@gmail.com>
2020-10-09 20:37:15 -0300

    Removed Row with ListView

--- lib/src/routes/home/Category.dart
+++ lib/src/routes/home/Category.dart
@@ -19,9 +19,9 @@ class Category extends StatefulWidget {
 class _CategoryState extends State<Category> {
   //---- Variables
 
-  Map category = {"name": ""};
-
   List produtosDaCategoriaEscolhida = [];
+
+  Map category = {"name": ""};
 
   //---- Functions
 
@@ -49,142 +49,85 @@ class _CategoryState extends State<Category> {
     return Scaffold(
       backgroundColor: Colors.green,
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Divider(
-              color: Colors.green,
-              height: 40,
-            ),
-            Row(
-              children: [
-                IconButton(
-                    icon: Icon(
-                      Icons.arrow_back,
-                      color: Colors.white,
-                      size: 34,
-                    ),
-                    onPressed: () => Navigator.pop(context)),
-                Divider(
-                  indent: size.width * 0.3,
-                ),
-                Text(
-                  category["name"],
-                  style: TextStyle(color: Colors.white, fontSize: 24),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  width: 200,
-                  height: 1000,
-                  child: ListView.builder(
-                    itemBuilder: (context, index) {
-                      return GestureDetector(
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(20),
-                          child: Container(
-                              width: 1000,
-                              height: size.height * 0.32,
-                              child: Card(
-                                  color: Colors.white,
-                                  child: Column(
-                                    children: [
-                                      Container(
-                                        width: 100,
-                                        height: 130,
-                                        child: Image.file(
-                                          File(produtosDaCategoriaEscolhida[
-                                              index]["image"][0]),
-                                          fit: BoxFit.fill,
-                                          filterQuality: FilterQuality.high,
-                                        ),
-                                      ),
-                                      ListTile(
-                                        title: Text(
-                                            produtosDaCategoriaEscolhida[index]
-                                                ["title"]),
-                                        subtitle: Text(
-                                            produtosDaCategoriaEscolhida[index]
-                                                ["subtitle"]),
-                                        trailing: Text(
-                                          "R\$25,00",
-                                          style: TextStyle(color: Colors.green),
-                                        ),
-                                      )
-                                    ],
-                                  ))),
-                        ),
-                        onTap: () => Navigator.push(
-                            context,
-                            PageTransition(
-                                child: Product(
-                                  item: produtosDaCategoriaEscolhida[index],
-                                ),
-                                type: PageTransitionType.rightToLeft)),
-                      );
-                    },
-                    itemCount: produtosDaCategoriaEscolhida.length,
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Divider(
+            color: Colors.green,
+            height: 40,
+          ),
+          Row(
+            children: [
+              IconButton(
+                  icon: Icon(
+                    Icons.arrow_back,
+                    color: Colors.white,
+                    size: 34,
                   ),
-                ),
-                Container(
-                  width: 200,
-                  height: 1000,
-                  child: ListView.builder(
-                    itemBuilder: (context, index) {
-                      return GestureDetector(
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(20),
-                          child: Container(
-                              width: 1000,
-                              height: size.height * 0.32,
-                              child: Card(
-                                  color: Colors.white,
-                                  child: Column(
-                                    children: [
-                                      Container(
-                                        width: 100,
-                                        height: 100,
-                                        child: Image.file(
-                                          File(produtosDaCategoriaEscolhida[
-                                              index]["image"][0]),
-                                          fit: BoxFit.fill,
-                                          filterQuality: FilterQuality.high,
-                                        ),
-                                      ),
-                                      ListTile(
-                                        title: Text(
-                                            produtosDaCategoriaEscolhida[index]
-                                                ["title"]),
-                                        subtitle: Text(
-                                            produtosDaCategoriaEscolhida[index]
-                                                ["subtitle"]),
-                                        trailing: Text(
-                                          "R\$25,00",
-                                          style: TextStyle(color: Colors.green),
-                                        ),
-                                      )
-                                    ],
-                                  ))),
-                        ),
-                        onTap: () => Navigator.push(
-                            context,
-                            PageTransition(
-                                child: Product(
-                                  item: produtosDaCategoriaEscolhida[index],
+                  onPressed: () => Navigator.pop(context)),
+              Divider(
+                indent: size.width * 0.25,
+              ),
+              Text(
+                category["name"],
+                style: TextStyle(color: Colors.white, fontSize: 24),
+              ),
+            ],
+          ),
+          Container(
+            width: 200,
+            height: 1000,
+            child: ListView.builder(
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Container(
+                        width: 1000,
+                        height: size.height * 0.32,
+                        child: Card(
+                            color: Colors.white,
+                            child: Column(
+                              children: [
+                                Container(
+                                  width: 100,
+                                  height: 130,
+                                  child: Image.file(
+                                    File(produtosDaCategoriaEscolhida[index]
+                                        ["image"][0]),
+                                    fit: BoxFit.fill,
+                                    filterQuality: FilterQuality.high,
+                                  ),
                                 ),
-                                type: PageTransitionType.rightToLeft)),
-                      );
-                    },
-                    itemCount: produtosDaCategoriaEscolhida.length,
+                                ListTile(
+                                  title: Text(
+                                      produtosDaCategoriaEscolhida[index]
+                                          ["title"]),
+                                  subtitle: Text(
+                                      produtosDaCategoriaEscolhida[index]
+                                          ["subtitle"]),
+                                  trailing: Text(
+                                    "R\$25,00",
+                                    style: TextStyle(color: Colors.green),
+                                  ),
+                                )
+                              ],
+                            ))),
                   ),
-                ),
-              ],
-            )
-          ],
-        ),
-      ),
+                  onTap: () => Navigator.push(
+                      context,
+                      PageTransition(
+                          child: Product(
+                            item: produtosDaCategoriaEscolhida[index],
+                          ),
+                          type: PageTransitionType.rightToLeft)),
+                );
+              },
+              itemCount: produtosDaCategoriaEscolhida.length,
+            ),
+          ),
+        ],
+      )),
     );
   }
 }
