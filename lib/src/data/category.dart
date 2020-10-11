8a5e772de736b950e9ebbea8d52c013b3e44819d
Ramon Paolo Maran <ramonpaolomaran12@gmail.com>
2020-10-10 21:02:01 -0300

    Att name

--- lib/src/data/category.dart
+++ lib/src/data/category.dart
@@ -1,7 +1,7 @@
 List category = [
   {"name": "Cosméticos", "image": "assets/images/cosmeticos.jpg"},
   {"name": "Adubos", "image": "assets/images/adubo.jpg"},
-  {"name": "Ferramenta", "image": "assets/images/ferramenta.jpg"},
+  {"name": "Ferramentas", "image": "assets/images/ferramenta.jpg"},
   {"name": "Eletrônicos", "image": "assets/images/drone.jpg"},
   {"name": "Ter na casa", "image": "assets/images/ter_casa.webp"},
 ];
